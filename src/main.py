0f2a7454d956ff927f9205e593b0285ec188a23b
shadawcraw <alexandre1920911@gmail.com>
2022-02-27 16:18:48 +0200

    Add docstrings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
+"""Main entry point for the game."""
+
 def main():
+    """Main entry point for the game.
+
+    Raises:
+        NotImplementedError: Placeholder for now
+    """
     raise NotImplementedError  # Placeholder
 
 
